7b7f494ce09587ca88ce6ecf1722da2b496b1b26
Ana Paula <anapaulavnoronha@gmail.com>
2016-08-09 18:16:26 -0300

    Remover Cartao + Atualizacao do Layout

--- js/index.js
+++ js/index.js
@@ -1,10 +1,34 @@
+var numCartoes = $('.cartao').length;
 $('#adicionarCartao').click(criaCartao);
 
 function criaCartao() {
 
+    numCartoes++;
+
     var novoConteudo = $('<p>').addClass('cartao-conteudo').attr('contenteditable', true);
+
+    var botaoRemove = $('<button>').addClass('remove-cartao').attr('data-cartao', 'cartao_' + numCartoes);
+    botaoRemove.click(removeCartao);
 
     var novoCartao = $('<div>').addClass('cartao');
 
-    novoCartao.append(novoConteudo).appendTo('.toDo');
+    novoCartao.append(novoConteudo);
+    novoCartao.append(botaoRemove);
+    novoCartao.attr('id', 'cartao_' + numCartoes);
+
+    novoCartao.prependTo('.coluna-cartoes');
+}
+
+//remover cartao
+$('.remove-cartao').click(removeCartao);
+
+function removeCartao() {
+    var idCartao = $(this).data('cartao');
+    var cartao = $('#' + idCartao);
+
+    cartao.addClass('cartao--some');
+
+    setTimeout(function () {
+        cartao.remove();
+    }, 500);
 }
